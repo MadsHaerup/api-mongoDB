659189a2ee2352374ebb67fe50cede3f3f8134cb
MadsHaerup <506094@RTS-365.dk>
2020-12-16 09:06:20 +0100

    cors activated

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 var formidable = require('express-formidable');
+var cors = require('cors');
 //configuration
 require('./database');
+app.use(cors());
 app.use(formidable());
 //route
 require('./cheeses.route')(app);
